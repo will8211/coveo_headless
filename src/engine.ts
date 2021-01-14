84dc1e2ecf8ad44ded349d8821f441b178f44d2b
William Mannard <wmannard@coveo.com>
2021-01-14 17:30:27 -0500

    Switched to sample org

--- src/engine.ts
+++ src/engine.ts
@@ -1,11 +1,6 @@
 import { HeadlessEngine, searchAppReducers } from "@coveo/headless";
 
 export const engine = new HeadlessEngine({
-  // configuration: HeadlessEngine.getSampleConfiguration(),
-  configuration: {
-    organizationId: 'williamonboarding1digvwql',
-    accessToken: 'xx42a566f8-e334-4300-bfc5-346171ebfa95',
-    platformUrl: 'https://platformqa.cloud.coveo.com'
-  },
+  configuration: HeadlessEngine.getSampleConfiguration(),
   reducers: searchAppReducers
 });
